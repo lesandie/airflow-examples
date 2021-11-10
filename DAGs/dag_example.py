93106155d19aacf2f5dc766e963f4d6745730f14
Diego Nieto <dnieto@tinybird.co>
2021-11-10 16:20:01 +0100

    first dag

--- DAGs/dag_example.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-from datetime import timedelta
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
-
