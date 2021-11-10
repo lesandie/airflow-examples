38b51d7030f201c0ee0acc091ffe942ab7a59038
Diego Nieto <dnieto@tinybird.co>
2021-11-10 16:09:10 +0100

    removed more logs

---
+++ DAGs/dag_example.py
@@ -0,0 +1,7 @@
+import datetime as dt
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+import pandas as pd
+
