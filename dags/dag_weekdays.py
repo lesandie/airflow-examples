b3189b9714566cefefce5ea0da6fdeb7912db74c
Diego Nieto (lesandie) <lesandie@gmail.com>
2022-03-08 23:26:17 +0100

    Updated dag

--- dags/dag_weekdays.py
+++ dags/dag_weekdays.py
@@ -1,27 +1,24 @@
-
-import logging
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
 
-log = logging.getLogger(__name__)
-
 def check_weekday(**kwargs):
+    """
+    Checks if the next execution date is a weekday
+    """
     next_execution_date = kwargs['data_interval_end']
-    log.info(f"next execution date is: {next_execution_date}")
+    print(f"Next execution date is: {next_execution_date}")
     date_check = next_execution_date.weekday()
-    log.info(f"date_check is: {date_check}")
-    # 0 Monday
+    # 0 Monday, 1 Tuesday ...
+    week_day = { 0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday" }
+    print(f": Next execution is scheduled on {week_day[date_check]}")
     if date_check in (0,1,2,3,4):
-        decision = True
+        return True
     else:
-        decision = False
-
-    log.info(f"decision is: {decision}")
-    return decision
+        return False
 
 def log_output(message):
-     log.info(f"{message}")
+    print(f"{message}")
 
 # DAG parameters
 default_args = {
@@ -38,26 +35,24 @@ dag_args = dict(
     #schedule_interval="@daily",
     schedule_interval=timedelta(minutes=1)
 )
+
 with DAG(**dag_args) as dag:
 
     start_dummy = PythonOperator(
         task_id='start',
         python_callable=log_output,
         op_args=['Start'],
-        dag=dag
     )
 
     end_dummy = PythonOperator(
         task_id='end',
         python_callable=log_output,
         op_args=['End'],
-        dag=dag
     )
 
     weekdays_only = ShortCircuitOperator(
         task_id='weekdays_only',
         python_callable=check_weekday,
-        dag=dag
     )
 
     start_dummy >> weekdays_only >> end_dummy
